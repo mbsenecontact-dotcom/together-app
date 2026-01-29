9ade05653a77b48425e4367ec0bbbaaf09a1e4bb
mame birame sene <mame-birame.sene@i-carre.net>
2026-01-29 16:15:04 +0100

    Correction et Amélioration

--- android/app/src/main/java/com/macsy/together/MainActivity.java
+++ android/app/src/main/java/com/macsy/together/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package com.macsy.together;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.macsy.together',
   appName: 'Together App',
   webDir: 'www',
   plugins: {
